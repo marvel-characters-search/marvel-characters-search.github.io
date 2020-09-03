d85060da40b67466827082e86cb1aacb6e18ccdd
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-02 21:17:23 -0700

    Created a container for characters that makes an api call to get 100 characters

--- src/containers/CharactersContainer.js
+++ src/containers/CharactersContainer.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import * as md5 from 'md5';
-
+import { signUrl } from '../urlSigner';
 
 class CharactersContainer extends React.Component {
+  state = {
+    characters: [],
+  }
+
+  componentDidMount() {
+    let url = signUrl(`http://gateway.marvel.com/v1/public/characters?limit=100`);
+    fetch(url)
+      .then(res => res.json())
+      .then(res => this.setState({
+        characters: res.data.results,
+      }))
+      .catch(err => console.log(err))
+  }
+
+  render() {    
+    return (
+      <>
+        <h1>Container</h1>
+      </>
+    )
+  }
 }
 
 export default CharactersContainer;
