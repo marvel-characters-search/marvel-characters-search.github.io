d1dfcb7721a5740e8f45ce2ed3e616316953a3fe
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-02 22:53:58 -0700

    Added code that defines property matchMedia for ant design

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+
+// sidestep https://github.com/ant-design/ant-design/issues/24906
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
