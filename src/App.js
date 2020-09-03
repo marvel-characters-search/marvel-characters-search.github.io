1f286e4507e2ba2a32eac7bc803012934ba7ed7e
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-03 14:43:40 -0700

    Added layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import './App.css';
 import "antd/dist/antd.css";
+import { Layout, Row, Col } from 'antd';
+
+
 
 import CharactersContainer from './containers/CharactersContainer';
-import Search from 'antd/lib/input/Search';
+import Search from './components/Search';
 
 function App() {
+  const { Header, Content, Footer } = Layout;
   return (
-    <div className="App">
-      <Search />
-      <CharactersContainer />
-    </div>
+    // <div className="App">
+    //   <Search />
+    //   {/* <CharactersContainer /> */}
+    // </div>
+
+    <Layout className="layout">
+      <Header />
+      <Content style={{ padding: '0 50px'}}>
+        <Row justify="center">
+          <Col span={12} offset={6}>
+            <Search />
+          </Col>
+          <Col span={24}>
+            <CharactersContainer />
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 }
 
