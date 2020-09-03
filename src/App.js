f3eda78f66f47e78e6ff7e8c76bc40ed6fcbd5eb
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-02 21:17:49 -0700

    Included characters container to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import CharactersContainer from './containers/CharactersContainer';
 
 function App() {
   return (
     <div className="App">
+      <CharactersContainer />
     </div>
   );
 }
