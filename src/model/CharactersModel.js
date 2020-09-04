3b38ec32614df27a40ef326172adbbdcc95ead02
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-04 00:58:00 -0700

    Added functions for searching character by name and nameStartsWith

--- src/model/CharactersModel.js
+++ src/model/CharactersModel.js
@@ -1,14 +1,28 @@
 import { signUrl } from '../urlSigner';
-let url = signUrl(`http://gateway.marvel.com/v1/public/characters?limit=18`);
 
 class CharactersModel {
   static getPaginatedCharacters = () => {
+    let url = signUrl(`http://gateway.marvel.com/v1/public/characters?limit=18`);
     return fetch(url)
       .then(res => res.json())
   };
 
-  // static getCharacterByName = () => {
-  // }
+  static getCharactersByNameStart = (nameStart) => {
+    if (nameStart) {
+      let url = signUrl(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameStart}`);
+      return fetch(url)
+        .then(res => res.json())
+    }
+  }
+
+  static getSelectedCharacter = (characterName) => {
+    if (characterName) {
+      let nameForUrl = characterName.split(" ").join("%20");
+      let url = signUrl(`http://gateway.marvel.com/v1/public/characters?name=${nameForUrl}`);
+      return fetch(url)
+        .then(res => res.json())
+    }
+  }
 }
 
 export default CharactersModel;
