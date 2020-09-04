20862858cfda7159ba736852e7a7481a1da25672
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-04 00:54:33 -0700

    Separated logic for rendering content into small functions

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
 import React from 'react';
-import { signUrl } from './urlSigner';
-import './App.css';
+import CharactersModel from './model/CharactersModel';
 import "antd/dist/antd.css";
-import { Layout, Row, Col } from 'antd';
-import CharactersContainer from './containers/CharactersContainer';
-import Search from './components/Search';
+import './App.css';
+import { Layout, Row, Col, PageHeader } from 'antd';
+import CharactersContainer from './containers/CharactersContainer/CharactersContainer';
+import Search from './components/Search/Search';
+import CharacterDetailContainer from './containers/CharacterDetailContainer/CharacterDetailContainer';
 
 class App extends React.Component {
   state = {
+    selectedCharacter: '',
     characters: [],
+    options: [],
   };
 
   componentDidMount() {
-    let url = signUrl(`http://gateway.marvel.com/v1/public/characters?limit=18`);
-    fetch(url)
-      .then(res => res.json())
-      .then(res => this.setState({
-        characters: res.data.results,
-      }))
+    this.renderPaginatedCharacters()
+  };
+
+  setMultipleOptions = (characters) => {
+    this.setState({
+      selectedCharacter: '',
+      characters: characters
+    })
+  };
+
+  renderSelectedCharacter = (characterName) => {
+    CharactersModel.getSelectedCharacter(characterName)
+      .then(res => {
+        this.setState({
+          selectedCharacter: res.data.results[0]
+        })
+      })
+  };
+
+  renderPaginatedCharacters = () => {
+    CharactersModel.getPaginatedCharacters()
+      .then(res => {
+        this.setMultipleOptions(res.data.results)
+      })
       .catch(err => console.log(err))
   };
 
   render() {
     const { Header, Content } = Layout;
+    let componentToRender;
+    if (this.state.selectedCharacter !== '') {
+      componentToRender =
+        <CharacterDetailContainer
+        renderPaginatedCharacters={this.renderPaginatedCharacters}
+        character={this.state.selectedCharacter}
+      />
+    } else {
+      componentToRender =
+        <CharactersContainer characters={this.state.characters} />
+    }
+
     return (
       <Layout className="layout">
         <Header />
         <Content style={{ padding: '0 50px' }}>
           <Row justify="center">
             <Col span={12}>
-              <Search />
+              <Search
+                renderSelectedCharacter={this.renderSelectedCharacter}
+                setMultipleOptions={this.setMultipleOptions}
+                renderPaginatedCharacters={this.renderPaginatedCharacters}
+              />
             </Col>
             <Col span={24}>
-              <CharactersContainer characters={this.state.characters}/>
+              {componentToRender}
             </Col>
           </Row>
         </Content>
