6770b36675950a44968f5f91337ffac52f77c3d1
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-05 20:52:27 -0700

    Added https instead of http to marvel link

--- src/model/CharactersModel.js
+++ src/model/CharactersModel.js
@@ -3,14 +3,14 @@ import { signUrl } from '../urlSigner';
 class CharactersModel {
   // Getting first 18 characters to render on the page
   static getPaginatedCharacters = () => {
-    let url = signUrl(`http://gateway.marvel.com/v1/public/characters?limit=18`);
+    let url = signUrl(`https://gateway.marvel.com/v1/public/characters?limit=18`);
     return fetch(url)
       .then(res => res.json())
   };
 
   static getCharactersByNameStart = (nameStart) => {
     if (nameStart) {
-      let url = signUrl(`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameStart}`);
+      let url = signUrl(`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${nameStart}`);
       return fetch(url)
         .then(res => res.json())
     }
@@ -19,7 +19,7 @@ class CharactersModel {
   static getSelectedCharacter = (characterName) => {
     if (characterName) {
       let nameForUrl = characterName.split(" ").join("%20");
-      let url = signUrl(`http://gateway.marvel.com/v1/public/characters?name=${nameForUrl}`);
+      let url = signUrl(`https://gateway.marvel.com/v1/public/characters?name=${nameForUrl}`);
       return fetch(url)
         .then(res => res.json())
     }
