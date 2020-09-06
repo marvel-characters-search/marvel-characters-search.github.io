bb4db528b1eb0d6e9cacfec9dc1be02c61ea762a
Yulia Tsernant <yulia.tsernant@d-d.me>
2020-09-05 20:21:54 -0700

    Updates

---
+++ precache-manifest.d9e04c2864e4f103bf6a881a131d9cf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6764184a91c998f877bf53f84178ccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60dee152cbd29acf9fdf",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "417312c4e19caac5cd63",
+    "url": "/static/css/main.c0b527cb.chunk.css"
+  },
+  {
+    "revision": "60dee152cbd29acf9fdf",
+    "url": "/static/js/2.c6918c83.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.c6918c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417312c4e19caac5cd63",
+    "url": "/static/js/main.a665798e.chunk.js"
+  },
+  {
+    "revision": "f694382c5b9e46b11a9d",
+    "url": "/static/js/runtime-main.0888e41a.js"
+  }
+]);
